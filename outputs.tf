f25f37ce62905fd74e519a8758e095533350b27e
atharfaraz <atharfaraz@live.com>
2023-03-27 18:28:01 +0000

    Add ip and dns in outputs

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,11 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+
+output "instance_association_public_ip" {
+  value = aws_instance.blog.association_public_ip
+}
+  
+output "instance_association_public_dns" {
+  value = aws_instance.blog.association_public_dns
+}
