8606c1e8b25c5ce1c5c058d5ac8846459461da4c
atharfaraz <atharfaraz@live.com>
2023-03-27 18:24:54 +0000

    missing security_group_id in rule

--- main.tf
+++ main.tf
@@ -45,6 +45,7 @@ resource "aws_security_group_rule" "blog_http_inbound" {
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
   
 }
 
@@ -54,6 +55,7 @@ resource "aws_security_group_rule" "blog_https_inbound" {
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
   
 }
 
@@ -63,5 +65,5 @@ resource "aws_security_group_rule" "blog_everything_outbound" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  
+  security_group_id = aws_security_group.blog.id
 }
