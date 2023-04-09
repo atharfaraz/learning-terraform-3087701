4a528f107c0c9a0aa511d3104d05c4b5d6a5b5d6
atharfaraz <atharfaraz@live.com>
2023-04-09 14:57:46 -0700

    Introduce ASG instead of instance

--- main.tf
+++ main.tf
@@ -32,19 +32,26 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+module "blog_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  # insert the 1 required variable here
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier  = module.blog_vpc.public_subnets
+  target_groups_arns = module.blog_alb.target_groups_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
 }
 
-
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -55,22 +62,6 @@ module "alb" {
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-
-      }
-    }
-  ]
 
 
   http_tcp_listeners = [
--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
-
-output "instance_public_ip" {
-  value = aws_instance.blog.public_ip
-}
 
 output "instance_public_dns" {
   value = aws_instance.blog.public_dns
